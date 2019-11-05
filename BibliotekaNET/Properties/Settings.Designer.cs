f81e231d132a44616b8d5ff1502a650848665c42
Łukasz Maciejczyk <lukasz482@gmail.com>
2019-11-05 17:44:49 +0100

    Removing unnecessary settings

--- BibliotekaNET/Properties/Settings.Designer.cs
+++ BibliotekaNET/Properties/Settings.Designer.cs
@@ -33,16 +33,5 @@ namespace BibliotekaNET.Properties {
                 return ((string)(this["BibliotekaConnectionString"]));
             }
         }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=lmaci.database.windows.net;Initial Catalog=Biblioteka;Persist Securit" +
-            "y Info=True;User ID=maci;Password=\"1939459989 Fe57a\"")]
-        public string StaryConnection {
-            get {
-                return ((string)(this["StaryConnection"]));
-            }
-        }
     }
 }
